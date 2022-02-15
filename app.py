7b57ebd7f76380060ffc8825ae4031706e20eff9
Daniel Morala <d645damo@systemair.se>
2022-02-15 11:30:23 +0100

    Añadidos comentarios

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
 from reportlab.pdfgen import canvas
 
+#Tamaño de hojas
 A4 = [841, 595]
 A5 = [595, 420]
 
 weight = 841
 height = 595
 
+#Input por parte de usuario
 co = input ("Número de pedido: ")
 mo = input ("Número de MO: ")
 linea = input ("Linea: ")
 modelo = input ("Modelo: ")
 pais = input ("País: ")
 
+
+#Títulos de carteles
 title = "PANELES Y PUERTAS"
 title2 = "PERFILES"
 title3 = "CARPETA"
 
+#Creación de cartel para puertas y paneles
 pdf_Puertas = canvas.Canvas(co + "_" + linea + "_" + "PUERTAS" + ".pdf", pagesize=A4)
 
 pdf_Puertas.setFontSize(66)
@@ -28,7 +33,7 @@ pdf_Puertas.setFontSize(30)
 pdf_Puertas.drawCentredString(weight/2, height - 580, pais)
 pdf_Puertas.save()
 
-
+#Creación de cartel para perfiles
 pdf_Perfiles = canvas.Canvas(
     co + "_" + linea + "_" + title2 + ".pdf", pagesize=A4)
 
@@ -42,6 +47,7 @@ pdf_Perfiles.setFontSize(30)
 pdf_Perfiles.drawCentredString(weight/2, height - 580, pais)
 pdf_Perfiles.save()
 
+#Creación de cartel para arpeta
 pdf_Carpeta = canvas.Canvas(
     co + "_" + linea + "_" + title3 + ".pdf", pagesize=A5)
 
