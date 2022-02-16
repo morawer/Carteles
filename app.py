42103317d8a1a775e230a73f9d2d91147f8449e5
Daniel Morala <morala84@gmail.com>
2022-02-16 14:21:44 +0100

    Creados carteles y hojas excel

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from reportlab.pdfgen import canvas
 import pandas as pd
+import os
+import openpyxl
 
 #Tamaño de hojas
 A4 = [841, 595]
@@ -11,11 +13,22 @@ height = 595
 weight2 = 595
 height2 = 841
 
+pathDestino = "U:/OPERACIONES/08 FÁBRICA/1 AUTOMATIZACIÓN CARTELES TRABAJO JEFES TURNO/"
+
+if not os.path.exists(pathDestino):
+    os.makedirs(pathDestino)
+
 #Input a traves de un archivo de Excel
 
 excel = "SEGUIMIENTO_PEDIDOS.xlsm"
 
 df = pd.read_excel(io= "SEGUIMIENTO_PEDIDOS.xlsm", sheet_name= "DV")
+
+excel_observaciones = openpyxl.load_workbook("OBSERVACIONES PEDIDOS.xlsx")
+sheet1_observaciones = excel_observaciones.active
+
+excel_protocolo = openpyxl.load_workbook("CL - Autocontrol Producción DV - Ed. 02.xlsx")
+sheet1_protocolo = excel_protocolo.active
 
 #Títulos de carteles
 title = "PANELES Y PUERTAS"
@@ -26,12 +39,13 @@ co = df["Unit"].values
 mo = df["MO no"].values
 modelo = df["CO Item no"].values
 status = df["MO sts"].values
+fecha = df["MO Start"].values
 
 for line in range(len(mo)):
     if status[line] != "90-90":
         moFloat = f"MO: {mo[line]:.0f}"
         #Creación de cartel para puertas y paneles
-        pdf_Puertas = canvas.Canvas(co[line] + "_" + "PUERTAS" + ".pdf", pagesize=A4)
+        pdf_Puertas = canvas.Canvas(pathDestino + co[line] + "_" + "PUERTAS" + ".pdf", pagesize=A4)
 
         pdf_Puertas.setFontSize(66)
         pdf_Puertas.drawCentredString(weight/2, height - 100, title)
@@ -45,7 +59,7 @@ for line in range(len(mo)):
 
         #Creación de cartel para perfiles
         pdf_Perfiles = canvas.Canvas(
-            co[line] + "_" + title2 + ".pdf", pagesize=A4)
+            pathDestino + co[line] + "_" + title2 + ".pdf", pagesize=A4)
 
         pdf_Perfiles.setFontSize(66)
         pdf_Perfiles.drawCentredString(weight/2, height - 100, title2)
@@ -59,7 +73,7 @@ for line in range(len(mo)):
 
         #Creación de cartel para arpeta
         pdf_Carpeta = canvas.Canvas(
-            co[line] + "_" + title3 + ".pdf", pagesize=A4Carpeta)
+            pathDestino + co[line] + "_" + title3 + ".pdf", pagesize=A4Carpeta)
 
         pdf_Carpeta.setFontSize(56)
         pdf_Carpeta.drawCentredString(weight2/2, height2-60, "CO: " + co[line])
@@ -67,5 +81,22 @@ for line in range(len(mo)):
         pdf_Carpeta.drawCentredString(weight2/2, height2-270, moFloat)
         #pdf_Carpeta.drawCentredString(595/2, 420-390, pais)
         pdf_Carpeta.save()
+
+        celdaA4_observaciones = sheet1_observaciones.cell(row=4, column=1)
+        celdaA4_observaciones.value = co[line]
+        celdaC4_observaciones = sheet1_observaciones.cell(row=4, column=3)
+        celdaC4_observaciones.value = modelo[line]
+        celdaD4_observaciones = sheet1_observaciones.cell(row=4, column=4)
+        celdaD4_observaciones.value = mo[line]
+        excel_observaciones.save(pathDestino + co[line] + "_OBSERVACIONES.xlsx")
+
+        celdaB4_protocolo = sheet1_protocolo.cell(row=4, column=2)
+        celdaB4_protocolo.value = co[line]
+        celdaC4_protocolo = sheet1_protocolo.cell(row= 4, column=4)
+        celdaC4_protocolo.value = moFloat
+        celdaB5_protocolo = sheet1_protocolo.cell(row=5, column=2)
+        celdaB5_protocolo.value = modelo[line]
+        
+        excel_protocolo.save(pathDestino + co[line] + "_PROTOCOLO.xlsx")
 
         print(co[line] + " >> " + moFloat + " >> " + modelo [line])
